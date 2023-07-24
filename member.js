a60e5156a568231cce64b808ac88afcff1b8189b
StackOfBiscuits <140396091+StackOfBiscuits@users.noreply.github.com>
2023-07-24 12:40:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skilsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'Sang',
+        age: 20,
+        skills: skills,
+        addSkill: function (skill) {
+            this.skills.push(skill);
+        }
+    };
+    member.addSkill('PHP');
+    console.log(member);
+}
